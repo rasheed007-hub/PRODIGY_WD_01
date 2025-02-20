44c8178dc170f05fbba621d762cdefb5f309e64b
rasheed <rasheedoduola007@gamil.com>
2025-02-20 15:16:31 +0100

    tab view

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const downDropMenu = document.querySelector('.dropdown_menu')
+
+toggleBtn.onclick = function () {
+    downDropMenu.classList.toggle('open')
+
+}
